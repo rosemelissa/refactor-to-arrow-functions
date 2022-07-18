e0ee7758911e8c4f037996a90e84d5bf095abc0f
Melissa Rose <123melissa321@gmail.com>
2022-07-18 10:27:59 +0100

    Completed Selene exercises

--- src/add.ts
+++ src/add.ts
@@ -5,9 +5,6 @@
  * @param secondNum second number to add
  * @returns total of the two numbers
  */
-function add(firstNum: number, secondNum: number): number {
-  const total = firstNum + secondNum;
-  return total;
-}
+const add = (firstNum: number, secondNum: number): number => firstNum + secondNum;
 
 export default add;
